d2bc618c598a5f2ab92c50aa71e692df0e50f4f5
JamieMason <siunmaru@gmail.com>
2013-04-04 15:11:33 +0100

    Added Testacular/Karma test runner

---
+++ karma.conf.js
@@ -0,0 +1,16 @@
+files = [
+  JASMINE,
+  JASMINE_ADAPTER,
+  'jasmine-matchers-1.3.0.js',
+  'jasmine-matchers.test.js'
+];
+
+exclude = [
+  'karma.conf.js'
+];
+
+autoWatch = true;
+
+reporters = ['progress'];
+
+port = 7357;
