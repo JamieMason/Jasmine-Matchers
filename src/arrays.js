f6b23020860151db1dbbffa20e709b710e0a9a3c
Jamie Mason <siunmaru@gmail.com>
2014-07-17 20:19:38 +0100

    JSDoc Array Matchers.

--- src/arrays.js
+++ src/arrays.js
@@ -1,6 +1,12 @@
   // Arrays
   // ---------------------------------------------------------------------------
 
+  /**
+   * @inner
+   * @param {String} toBeX
+   * @return {Function}
+   * @memberOf priv
+   */
   priv.createToBeArrayOfXsMatcher = function(toBeX) {
     return function() {
       return matchers.toBeArray.call(this) && priv.expectAllMembers.call(this, toBeX);
@@ -8,8 +14,15 @@
   };
 
   /**
-   * Assert subject is an Array (from this document, eg Arrays from iframes
-   * or popups won't match)
+   * @name toBeArray
+   *
+   * @description
+   * Assert subject is a true Array, created in the parent document — those created and imported
+   * from within iframes or other windows will not match.
+   *
+   * @example
+   * expect(["Foo", "Bar"]).toBeArray();
+   *
    * @return {Boolean}
    */
   matchers.toBeArray = function() {
@@ -17,8 +30,15 @@
   };
 
   /**
-   * Assert subject is an Array with a defined number of members
-   * @param  {Number} size
+   * @name toBeArrayOfSize
+   *
+   * @description
+   * Assert subject is not only a true Array, but one with a specific number of members.
+   *
+   * @example
+   * expect(["Foo", "Bar"]).toBeArrayOfSize(2);
+   *
+   * @param {Number} size
    * @return {Boolean}
    */
   matchers.toBeArrayOfSize = function(size) {
@@ -26,7 +46,14 @@
   };
 
   /**
-   * Assert subject is an Array, but with no members
+   * @name toBeEmptyArray
+   *
+   * @description
+   * Assert subject is not only a true Array, but one without any members.
+   *
+   * @example
+   * expect([]).toBeEmptyArray();
+   *
    * @return {Boolean}
    */
   matchers.toBeEmptyArray = function() {
@@ -34,7 +61,17 @@
   };
 
   /**
-   * Assert subject is an Array with at least one member
+   * @name toBeNonEmptyArray
+   *
+   * @description
+   * Assert subject is not only a true Array, but one with at least one member.
+   *
+   * @example
+   * expect(["Foo", "Bar"]).toBeNonEmptyArray();
+   *
+   * @example
+   * expect([]).not.toBeNonEmptyArray();
+   *
    * @return {Boolean}
    */
   matchers.toBeNonEmptyArray = function() {
@@ -42,25 +79,89 @@
   };
 
   /**
-   * Assert subject is an Array which is either empty or contains only Objects
+   * @name toBeArrayOfObjects
+   *
+   * @description
+   * Assert subject is an Array which is either empty or contains only Objects.
+   *
+   * @example
+   * expect([{}, {}]).toBeArrayOfObjects();
+   *
+   * @example
+   * // An empty Array of Objects is still considered valid .
+   * expect([]).toBeArrayOfObjects();
+   *
+   * @example
+   * // Although this Array contains Objects, it also contains other data types so is not
+   * considered valid.
+   * expect([{}, 12, {}]).not.toBeArrayOfObjects();
+   *
    * @return {Boolean}
    */
   matchers.toBeArrayOfObjects = priv.createToBeArrayOfXsMatcher('toBeObject');
 
   /**
-   * Assert subject is an Array which is either empty or contains only Strings
+   * @name toBeArrayOfStrings
+   *
+   * @description
+   * Assert subject is an Array which is either empty or contains only Strings.
+   *
+   * @example
+   * expect(["foo", ""]).toBeArrayOfStrings();
+   *
+   * @example
+   * // An empty Array of Strings is still considered valid .
+   * expect([]).toBeArrayOfStrings();
+   *
+   * @example
+   * // Although this Array contains Strings, it also contains other data types so is not
+   * considered valid.
+   * expect(["Governor", 12, "Marley"]).not.toBeArrayOfStrings();
+   *
    * @return {Boolean}
    */
   matchers.toBeArrayOfStrings = priv.createToBeArrayOfXsMatcher('toBeString');
 
   /**
-   * Assert subject is an Array which is either empty or contains only Numbers
+   * @name toBeArrayOfNumbers
+   *
+   * @description
+   * Assert subject is an Array which is either empty or contains only Numbers.
+   *
+   * @example
+   * expect([23, 98]).toBeArrayOfNumbers();
+   *
+   * @example
+   * // An empty Array of Numbers is still considered valid .
+   * expect([]).toBeArrayOfNumbers();
+   *
+   * @example
+   * // Although this Array contains Numbers, it also contains other data types so is not
+   * considered valid.
+   * expect([67, 38, {}]).not.toBeArrayOfNumbers();
+   *
    * @return {Boolean}
    */
   matchers.toBeArrayOfNumbers = priv.createToBeArrayOfXsMatcher('toBeNumber');
 
   /**
-   * Assert subject is an Array which is either empty or contains only Booleans
+   * @name toBeArrayOfBooleans
+   *
+   * @description
+   * Assert subject is an Array which is either empty or contains only Booleans.
+   *
+   * @example
+   * expect([true, false]).toBeArrayOfBooleans();
+   *
+   * @example
+   * // An empty Array of Booleans is still considered valid .
+   * expect([]).toBeArrayOfOBooleans);
+   *
+   * @example
+   * // Although this Array contains Booleans, it also contains other data types so is not
+   * considered valid.
+   * expect([true, false, null).not.toBeArrayOfBooleans();
+   *
    * @return {Boolean}
    */
   matchers.toBeArrayOfBooleans = priv.createToBeArrayOfXsMatcher('toBeBoolean');
