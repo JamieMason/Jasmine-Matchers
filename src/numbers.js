b2a7a082e013567ea39398dfab9cd512f50b2f01
Jamie Mason <siunmaru@gmail.com>
2014-07-17 22:43:49 +0100

    Add Number member matchers.

--- src/numbers.js
+++ src/numbers.js
@@ -52,3 +52,104 @@
   matchers.toBeWholeNumber = function() {
     return matchers.toBeNumber.call(this) && (this.actual === 0 || this.actual % 1 === 0);
   };
+
+  // Number Methods
+  // ---------------------------------------------------------------------------
+
+  /**
+   * @name toHaveNumber
+   *
+   * @description
+   * .
+   *
+   * @example
+   * See {@link http://git.io/jasmine-number-testing|Unit testing Numbers with Jasmine}.
+   *
+   * @param  {String} memberName
+   * @return {Boolean}
+   */
+  matchers.toHaveNumber = function(memberName) {
+    return priv.assertMember.call(this, 'toBeNumber', memberName);
+  };
+
+  /**
+   * @name toHaveNumberWithinRange
+   *
+   * @description
+   * .
+   *
+   * @example
+   * See {@link http://git.io/jasmine-number-testing|Unit testing Numbers with Jasmine}.
+   *
+   * @param  {String} memberName
+   * @param  {Number} floor
+   * @param  {Number} ceiling
+   * @return {Boolean}
+   */
+  matchers.toHaveNumberWithinRange = function(memberName, floor, ceiling) {
+    return priv.assertMember.call(this, 'toBeWithinRange', memberName, floor, ceiling);
+  };
+
+  /**
+   * @name toHaveCalculable
+   *
+   * @description
+   * .
+   *
+   * @example
+   * See {@link http://git.io/jasmine-number-testing|Unit testing Numbers with Jasmine}.
+   *
+   * @param  {String} memberName
+   * @return {Boolean}
+   */
+  matchers.toHaveCalculable = function(memberName) {
+    return priv.assertMember.call(this, 'toBeCalculable', memberName);
+  };
+
+  /**
+   * @name toHaveEvenNumber
+   *
+   * @description
+   * .
+   *
+   * @example
+   * See {@link http://git.io/jasmine-number-testing|Unit testing Numbers with Jasmine}.
+   *
+   * @param  {String} memberName
+   * @return {Boolean}
+   */
+  matchers.toHaveEvenNumber = function(memberName) {
+    return priv.assertMember.call(this, 'toBeEvenNumber', memberName);
+  };
+
+  /**
+   * @name toHaveOddNumber
+   *
+   * @description
+   * .
+   *
+   * @example
+   * See {@link http://git.io/jasmine-number-testing|Unit testing Numbers with Jasmine}.
+   *
+   * @param  {String} memberName
+   * @return {Boolean}
+   */
+  matchers.toHaveOddNumber = function(memberName) {
+    return priv.assertMember.call(this, 'toBeOddNumber', memberName);
+  };
+
+  /**
+   * @name toHaveWholeNumber
+   *
+   * @description
+   * .
+   *
+   * @example
+   * See {@link http://git.io/jasmine-number-testing|Unit testing Numbers with Jasmine}.
+   *
+   * @param  {String} memberName
+   * @return {Boolean}
+   */
+  matchers.toHaveWholeNumber = function(memberName) {
+    return priv.assertMember.call(this, 'toBeWholeNumber', memberName);
+  };
