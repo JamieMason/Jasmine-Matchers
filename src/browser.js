a8d8f597bb5552628fe847d937356a83b37d8e6b
Jamie Mason <jamie.mason@bskyb.com>
2014-07-18 09:55:37 +0100

    JSDoc Browser Member Matchers.

--- src/browser.js
+++ src/browser.js
@@ -77,3 +77,22 @@
   matchers.toBeHtmlCommentNode = function() {
     return priv.isHtmlElementOfType(this.actual, 8);
   };
+
+  // Browser Members
+  // ---------------------------------------------------------------------------
+
+  /**
+   * @name toBeHtmlNode
+   *
+   * @description
+   * Assert subject is a true Object containing a property at memberName which is an HTML Element.
+   *
+   * @example
+   * See {@link http://git.io/jasmine-browser-testing|Unit testing Browsers with Jasmine}.
+   *
+   * @param {Boolean} memberName
+   * @return {Boolean}
+   */
+  matchers.toHaveHtmlNode = function(memberName) {
+    return priv.assertMember.call(this, 'toBeHtmlNode', memberName);
+  };
