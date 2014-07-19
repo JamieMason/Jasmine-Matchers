68d3afaf967b3228251ab94b0d82af41e39b5fcc
Jamie Mason <siunmaru@gmail.com>
2014-07-19 18:13:19 +0100

    Get easy to read test failures with karma-nested-reporter.

--- karma.conf.js
+++ karma.conf.js
@@ -5,26 +5,18 @@ module.exports = function(config) {
   'use strict';
 
   config.set({
-
+    colors: true,
+    autoWatch: true,
     frameworks: ['jasmine'],
-
-    files: [
-      'dist/jasmine-matchers.js',
-      'test/*.test.js'
-    ],
-
+    reporters: ['nested', 'coverage'],
+    files: ['dist/jasmine-matchers.js', 'test/*.test.js'],
     preprocessors: {
       '**/dist/*.js': ['coverage']
     },
-
-    autoWatch: true,
-
-    reporters: ['progress', 'coverage'],
-
     coverageReporter: {
       type: 'html',
       dir: 'build/coverage/'
     }
-
   });
+
 };
