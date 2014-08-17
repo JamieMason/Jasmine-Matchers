6b3f75dd401827bf2ff9f6e300ac399d01df02da
Jamie Mason <siunmaru@gmail.com>
2014-08-17 21:08:47 +0100

    Refactor Object Matcher tests.

--- test/objects.test.js
+++ test/objects.test.js
@@ -1,49 +1,122 @@
 describe('Objects', function() {
 
+  beforeEach(function() {
+    this.Foo = function() {};
+  });
+
   describe('toBeObject', function() {
-    it('should assert value is an Object', function() {
-      function Foo(){}
-      expect(new Foo()).toBeObject();
-      expect({}).toBeObject();
-      expect(null).not.toBeObject();
+    describe('when invoked', function() {
+      describe('when subject IS an Object', function() {
+        it('should confirm', function() {
+          expect(new Object()).toBeObject();
+          expect(new this.Foo()).toBeObject();
+          expect({}).toBeObject();
+        });
+      });
+      describe('when subject is NOT an Object', function() {
+        it('should deny', function() {
+          expect(null).not.toBeObject();
+          expect(123).not.toBeObject();
+          expect('[object Object]').not.toBeObject();
+        });
+      });
     });
   });
 
   describe('toBeEmptyObject', function() {
-    it('should assert value is an Object with no instance members', function() {
-      function Foo(){}
-      Foo.prototype = { b: 2 };
-      expect({ a: 1 }).not.toBeEmptyObject();
-      expect(new Foo()).toBeEmptyObject();
-      expect({}).toBeEmptyObject();
-      expect(null).not.toBeNonEmptyObject();
+    describe('when invoked', function() {
+      describe('when subject IS an Object with no instance members', function() {
+        beforeEach(function() {
+          this.Foo.prototype = {
+            b: 2
+          };
+        });
+        it('should confirm', function() {
+          expect(new this.Foo()).toBeEmptyObject();
+          expect({}).toBeEmptyObject();
+        });
+      });
+      describe('when subject is NOT an Object with no instance members', function() {
+        it('should deny', function() {
+          expect({
+            a: 1
+          }).not.toBeEmptyObject();
+          expect(null).not.toBeNonEmptyObject();
+        });
+      });
     });
   });
 
   describe('toBeNonEmptyObject', function() {
-    it('should assert value is an Object with at least one instance member', function() {
-      function Foo(){}
-      Foo.prototype = { b: 2 };
-      expect({ a: 1 }).toBeNonEmptyObject();
-      expect(new Foo()).not.toBeNonEmptyObject();
-      expect({}).not.toBeNonEmptyObject();
-      expect(null).not.toBeNonEmptyObject();
+    describe('when invoked', function() {
+      describe('when subject IS an Object with at least one instance member', function() {
+        it('should confirm', function() {
+          expect({
+            a: 1
+          }).toBeNonEmptyObject();
+        });
+      });
+      describe('when subject is NOT an Object with at least one instance member', function() {
+        beforeEach(function() {
+          this.Foo.prototype = {
+            b: 2
+          };
+        });
+        it('should deny', function() {
+          expect(new this.Foo()).not.toBeNonEmptyObject();
+          expect({}).not.toBeNonEmptyObject();
+          expect(null).not.toBeNonEmptyObject();
+        });
+      });
     });
   });
 
   describe('toImplement', function() {
-    it('should assert value is an Object containing all of the supplied members', function() {
-      expect({ a: 1, b: 2 }).toImplement({ a: 1, b: 2 });
-      expect({ a: 1, b: 2 }).toImplement({ a: 1 });
-      expect({ a: 1 }).not.toImplement({ c: 3 });
-      expect(null).not.toImplement({ a: 1 });
+    describe('when invoked', function() {
+      describe('when subject IS an Object containing all of the supplied members', function() {
+        it('should confirm', function() {
+          expect({
+            a: 1,
+            b: 2
+          }).toImplement({
+            a: 1,
+            b: 2
+          });
+          expect({
+            a: 1,
+            b: 2
+          }).toImplement({
+            a: 1
+          });
+        });
+      });
+      describe('when subject is NOT an Object containing all of the supplied members', function() {
+        it('should deny', function() {
+          expect({
+            a: 1
+          }).not.toImplement({
+            c: 3
+          });
+          expect(null).not.toImplement({
+            a: 1
+          });
+        });
+      });
     });
   });
 
   describe('toBeFunction', function() {
-    it('should assert value is a function', function() {
-      expect(function(){}).toBeFunction();
-      expect(/regexp/).not.toBeFunction();
+    describe('when invoked', function() {
+      describe('when subject IS a function', function() {
+        it('should confirm', function() {
+          expect(function() {}).toBeFunction();
+        });
+      });
+      describe('when subject is NOT a function', function() {
+        it('should deny', function() {
+          expect(/regexp/).not.toBeFunction();
+        });
+      });
     });
   });
 
