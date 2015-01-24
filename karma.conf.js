864eeec86aa7d4249fce5e7211521cafa8a628fc
Jamie Mason <siunmaru@gmail.com>
2015-01-24 16:18:10 +0000

    Run Karma on all local os x browsers.

--- karma.conf.js
+++ karma.conf.js
@@ -3,29 +3,44 @@ module.exports = function(config) {
   'use strict';
 
   config.set({
+
     colors: true,
+
     autoWatch: true,
+
+    browsers: [
+      'Firefox',
+      'Opera',
+      'PhantomJS',
+      'Safari'
+    ],
+
     frameworks: [
       'jasmine'
     ],
+
     reporters: [
       'nested',
       'coverage'
     ],
+
     files: [
       'dist/jasmine-matchers.js',
       'test/**/_utils.test.js',
       'test/**/*.test.js'
     ],
+
     preprocessors: {
       '**/dist/*.js': [
         'coverage'
       ]
     },
+
     coverageReporter: {
       type: 'html',
       dir: 'build/coverage/'
     }
+
   });
 
 };
