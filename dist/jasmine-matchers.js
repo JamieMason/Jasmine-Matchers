967e72ffacab53ed30e4adc03a3071a7decec930
Jamie Mason <siunmaru@gmail.com>
2015-01-24 16:26:06 +0000

    Fix toBeIso8601 in Firefox.

--- dist/jasmine-matchers.js
+++ dist/jasmine-matchers.js
@@ -362,17 +362,52 @@
     return this.actual instanceof Date;
   };
 
+  function isIso8601(string, pattern) {
+    var returnValue = string.search(
+      new RegExp('^' + pattern.map(function(term) {
+        if (term === '-') {
+          return '\\-';
+        } else if (typeof term === 'string') {
+          return term;
+        } else {
+          return '([0-9]{' + term + '})';
+        }
+      }).join('') + '$')
+    ) !== -1;
+    return returnValue;
+  }
+
   /**
    * @alias    toBeIso8601
    * @summary  <code>expect(string).toBeIso8601();</code>
    */
   matchers.toBeIso8601 = function() {
-    return matchers.toBeString.call(this) &&
-      this.actual.length >= 10 &&
-      new Date(this.actual).toString() !== 'Invalid Date' &&
-      new Date(this.actual)
-      .toISOString()
-      .slice(0, this.actual.length) === this.actual;
+
+    if (!matchers.toBeString.call(this)) {
+      return false;
+    }
+
+    if (isIso8601(this.actual, [
+        // 2013-07-08
+        4, '-', 2, '-', 2
+      ]) || isIso8601(this.actual, [
+        // 2013-07-08T07:29
+        4, '-', 2, '-', 2, 'T', 2, ':', 2
+      ]) || isIso8601(this.actual, [
+        // 2013-07-08T07:29:15
+        4, '-', 2, '-', 2, 'T', 2, ':', 2, ':', 2
+      ]) || isIso8601(this.actual, [
+        // 2013-07-08T07:29:15.863
+        4, '-', 2, '-', 2, 'T', 2, ':', 2, ':', 2, '.', 3
+      ]) || isIso8601(this.actual, [
+        // 2013-07-08T07:29:15.863Z
+        4, '-', 2, '-', 2, 'T', 2, ':', 2, ':', 2, '.', 3, 'Z'
+      ])) {
+      return new Date(this.actual).toString() !== 'Invalid Date';
+    }
+
+    return false;
+
   };
 
   /**
