b8cb0639dba0edd57faabfa0b6654b18495455d2
Jamie Mason <siunmaru@gmail.com>
2015-01-28 19:21:46 +0000

    Only test versions of IE not available locally.

--- karma-saucelabs-ie.conf.js
+++ karma-saucelabs-ie.conf.js
@@ -8,30 +8,6 @@ module.exports = function(config) {
       browserName: 'internet explorer',
       platform: 'Windows 8.1',
       version: '11'
-    },
-    'sl_ie_10': {
-      base: 'SauceLabs',
-      browserName: 'iehta',
-      platform: 'Windows 7',
-      version: '10'
-    },
-    'sl_ie_9': {
-      base: 'SauceLabs',
-      browserName: 'iehta',
-      platform: 'Windows 7',
-      version: '9'
-    },
-    'sl_ie_8': {
-      base: 'SauceLabs',
-      browserName: 'iehta',
-      platform: 'Windows XP',
-      version: '8'
-    },
-    'sl_ie_7': {
-      base: 'SauceLabs',
-      browserName: 'iehta',
-      platform: 'Windows XP',
-      version: '7'
     }
   };
 
