ea5ed1d4bf54364744d355c43868ec8a5347f8f1
Jamie Mason <siunmaru@gmail.com>
2015-02-23 20:40:11 +0000

    Change jasmine version check, fixes #29.

--- dist/jasmine-matchers.js
+++ dist/jasmine-matchers.js
@@ -941,15 +941,22 @@
     return priv.assertMember.call(this, 'toBeWhitespace', key);
   };
 
-  // Create adapters for the original matchers so they can be compatible with Jasmine 2.0.
-  var matchersV2 = priv.adaptMatchers(matchers);
+  if (typeof jasmine.addMatchers === 'function') {
 
-  beforeEach(function() {
-    if (typeof this.addMatchers === 'function') {
-      this.addMatchers(matchers);
-    } else if (typeof jasmine.addMatchers === 'function') {
+    // Create adapters for the original matchers so
+    // they can be compatible with Jasmine 2.0.
+    var matchersV2 = priv.adaptMatchers(matchers);
+
+    beforeEach(function() {
       jasmine.addMatchers(matchersV2);
-    }
-  });
+    });
+
+  } else {
+
+    beforeEach(function() {
+      this.addMatchers(matchers);
+    });
+
+  }
 
 }());
--- src/wrapper/add-matchers-adapter.js
+++ src/wrapper/add-matchers-adapter.js
@@ -1,10 +1,17 @@
-// Create adapters for the original matchers so they can be compatible with Jasmine 2.0.
-var matchersV2 = priv.adaptMatchers(matchers);
+if (typeof jasmine.addMatchers === 'function') {
 
-beforeEach(function() {
-  if (typeof this.addMatchers === 'function') {
-    this.addMatchers(matchers);
-  } else if (typeof jasmine.addMatchers === 'function') {
+  // Create adapters for the original matchers so
+  // they can be compatible with Jasmine 2.0.
+  var matchersV2 = priv.adaptMatchers(matchers);
+
+  beforeEach(function() {
     jasmine.addMatchers(matchersV2);
-  }
-});
+  });
+
+} else {
+
+  beforeEach(function() {
+    this.addMatchers(matchers);
+  });
+
+}
