c5bc87570859594573b8f6f6db91af7522826c83
Jamie Mason <siunmaru@gmail.com>
2016-07-24 19:26:40 +0100

    feat(numbers): add toBeLessThanOrEqualTo

---
+++ src/toBeLessThanOrEqualTo.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var toBeNumber = require('./toBeNumber');
+
+module.exports = toBeLessThanOrEqualTo;
+
+function toBeLessThanOrEqualTo(otherNumber, actual) {
+  return toBeNumber(actual) && actual <= otherNumber;
+}
---
+++ test/toBeLessThanOrEqualTo.spec.js
@@ -0,0 +1,13 @@
+'use strict';
+
+describe('toBeLessThanOrEqualTo', function () {
+  it('asserts value is less or equal than a given number', function () {
+    expect(1).toBeLessThanOrEqualTo(2);
+    expect(-1).toBeLessThanOrEqualTo(1);
+    expect(-2).toBeLessThanOrEqualTo(-1);
+    expect(-2).toBeLessThanOrEqualTo(-2);
+    expect(NaN).not.toBeLessThanOrEqualTo(0);
+    expect(2).not.toBeLessThanOrEqualTo(1);
+    expect(0).not.toBeLessThanOrEqualTo(-1);
+  });
+});
