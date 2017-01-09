4eeeef47440bbf2f3479a5b86bb0cc43d17c8ca8
Jacob Ward <jacob@candyboxmedia.com>
2017-01-09 22:51:15 +0000

    Add spec for toBeRegExp()

---
+++ test/toBeRegExp.spec.js
@@ -0,0 +1,13 @@
+// spec
+describe('toBeRegExp', function () {
+  describe('when invoked', function () {
+    describe('when value is an instance of RegExp', function () {
+      it('should confirm', function () {
+        expect(new RegExp()).toBeRegExp();
+      });
+    });
+    describe('when value is not an instance of RegExp', function () {
+      expect(null).not.toBeRegExp();
+    });
+  });
+});
