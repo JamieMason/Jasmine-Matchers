f8b0152a5bd691b77bd163c97207f44fc2fa3845
Jacob Ward <jacobwarduk@users.noreply.github.com>
2017-01-17 17:37:33 +0000

    Revert "Feature/to be near"

--- src/toBeNear.js
+++
@@ -1,7 +0,0 @@
-// modules
-var toBeNumber = require('./toBeNumber');
-
-// public
-module.exports = function toBeNear(number, epsilon, actual) {
-  return toBeNumber(actual) && actual >= number - epsilon && actual <= number + epsilon;
-};
