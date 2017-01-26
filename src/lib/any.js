e6fd54643457b606de1a22634b1c955aa7fc83cc
Jose M Andres <jmandreslopez@users.noreply.github.com>
2017-01-26 13:59:09 -0600

    Wrong function name

--- src/lib/any.js
+++ src/lib/any.js
@@ -1,5 +1,5 @@
 // public
-module.exports = function every(array, truthTest) {
+module.exports = function any(array, truthTest) {
   for (var i = 0, len = array.length; i < len; i++) {
     if (truthTest(array[i])) {
       return true;
