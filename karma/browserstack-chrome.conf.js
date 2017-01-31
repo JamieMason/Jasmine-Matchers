71deda5f1115f32c37142704954867f9c23f653f
Jamie Mason <jamie@foldleft.io>
2017-01-31 12:42:35 +0000

    test(saucelabs): reduce list of browsers tested against

--- karma/browserstack-chrome.conf.js
+++ karma/browserstack-chrome.conf.js
@@ -14,37 +14,10 @@ module.exports = function (config) {
         'chrome-47.0',
         'chrome-46.0',
         'chrome-45.0',
-        'chrome-44.0',
         'chrome-43.0',
         'chrome-42.0',
         'chrome-41.0',
-        'chrome-40.0',
-        'chrome-39.0',
-        'chrome-38.0',
-        'chrome-37.0',
-        'chrome-36.0',
-        'chrome-35.0',
-        'chrome-34.0',
-        'chrome-33.0',
-        'chrome-32.0',
-        'chrome-31.0',
-        'chrome-30.0',
-        'chrome-29.0',
-        'chrome-28.0',
-        'chrome-27.0',
-        'chrome-26.0',
-        'chrome-25.0',
-        'chrome-24.0',
-        'chrome-23.0',
-        'chrome-22.0',
-        'chrome-21.0',
-        'chrome-20.0',
-        'chrome-19.0',
-        'chrome-18.0',
-        'chrome-17.0',
-        'chrome-16.0'
-        // 'chrome-15.0' // browserstack runner is showing choose search engine screen
-        // 'chrome-14.0' // browserstack reporting version is invalid
+        'chrome-40.0'
       ];
       config.set(base);
     }
--- karma/browserstack-ios.conf.js
+++ karma/browserstack-ios.conf.js
@@ -6,15 +6,10 @@ module.exports = function (config) {
   baseConfig({
     set: function (base) {
       base.browsers = [
-        'iphone-9.1',
         'ipad-9.1',
-        'iphone-8.3',
         'ipad-8.3',
-        'iphone-7.0',
         'ipad-7.0',
-        'iphone-6.0',
         'ipad-6.0'
-        // 'iphone-5.1', // does not support new Date('ISO 8601')
         // 'ipad-5.1', // does not support new Date('ISO 8601')
         // 'ipad-5.0' // browser does not launch
       ];
